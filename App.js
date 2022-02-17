94ee0e3420d6185a9ca217d3a1658208823117af
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-02-17 16:46:58 +0000

    installed react-icons and created text input

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
-import { borderLeftColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity } from 'react-native'
+inport {GrAdd} from 'react-icons/gr'
 import { Task } from './components/Task';
 
 export default function App() {
@@ -19,7 +19,19 @@ export default function App() {
       < Task text={'Task 2'}/>
      </View>
 
-    <KeyboardAvoidingView></KeyboardAvoidingView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? "padding" : "height"}
+      style = {styles.textInputContainer}>
+          <TextInput style={styles.textInput} placeholder={'Write a to-do'}>
+
+          </TextInput>
+
+          <TouchableOpacity>
+            <View style ={styles.addButtonContainer}>
+              <Text style={styles.addButton}>Add</Text>
+            </View>
+          </TouchableOpacity>
+    </KeyboardAvoidingView>
 
     </View>
   );
@@ -43,4 +55,35 @@ sectionTitle: {
 items: {
   marginTop: 30,
 },
+textInputContainer: {
+  position: 'absolute',
+  bottom: 60,
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+},
+textInput: {
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  paddingLeft: 15,
+  backgroundColor: 'white',
+  borderRadius: 60,
+  borderColor: '#C0C0C0',
+  borderWidth: 2,
+  width: 250,
+
+},
+addButtonContainer: {
+  width: 60,
+  height: 60,
+  backgroundColor: 'white',
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: '#C0C0C0',
+  borderWidth: 2,
+
+},
+addButton: {},
 });
